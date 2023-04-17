d85e37beb27d1ee53672e5bccc49ebb393233c20
Justin Davis <jrcd83@gmail.com>
2023-04-16 20:37:55 -0400

    gerlfmt: Make a quick command entrypoint

---
+++ gerlfmt.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"fmt"
+	"os"
+)
+
+func main() {
+	buf, _ := io.ReadAll(os.Stdin)
+	items := lexErlTerm("stdin", string(buf))
+	if err := prettyErlTerm(items, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+	os.Stdout.Sync()
+}
